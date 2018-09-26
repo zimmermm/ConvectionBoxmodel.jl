f8c8eccaf61bd21c4fb2a92138e82c54dc9a449f
Matthias Zimmermann <zimmermm@hotmail.com>
2018-09-26 19:50:44 +0200

    bugfix ceq_ch4: make physicsfn

--- src/convectionlakephysics.jl
+++ src/convectionlakephysics.jl
@@ -356,16 +356,16 @@ const bi = [0.8181, -3.85e-3, 4.96e-5]
 
 # Schmitt-Number
 Sc_ch4_Wanninkhof(T) = 1909.4 - 120.78*(T-273.15) + 4.1555*((T-273.15)^2) - 0.080578*((T-273.15)^3) + 0.00065777*((T-273.15)^4)
-Ceq_ch4(T,S) = exp(-68.8862 + 101.4956*(100/T) + 28.7314*log(T/100) + S*(-0.076146 + 0.043970*(T/100) - 0.006872*(T/100)^2))*(p0/(R*T0))*1.8e-6
+@physicsfn Ceq_ch4(p::ConvectionLakePhysics{<:DefaultPhysics},T,S) = exp(-68.8862 + 101.4956*(100/T) + 28.7314*log(T/100) + S*(-0.076146 + 0.043970*(T/100) - 0.006872*(T/100)^2))*(p0/(R*T0))*1.8e-6
 # Gas Transfer Velocity
 
 @physicsfn k(p::ConvectionLakePhysics{<:DefaultPhysics}, u,t) = η*(ϵ(p,u,t)*ν)^0.25*Sc_ch4_Wanninkhof(u[5])^(-0.5)
 @physicsfn k_u(p::ConvectionLakePhysics{<:DefaultPhysics}, u,t) = η*(ϵ_u(p,u,t)*ν)^0.25*Sc_ch4_Wanninkhof(u[5])^(-0.5)
 @physicsfn k_B(p::ConvectionLakePhysics{<:DefaultPhysics}, u,t) = η*(ϵ_B(p,u,t)*ν)^0.25*Sc_ch4_Wanninkhof(u[5])^(-0.5)
 
-@physicsfn Fatm(p::ConvectionLakePhysics{<:DefaultPhysics}, u,t) = -k(p,u,t)*(u[3]-Ceq_ch4(u[5],0)*1e6)
-@physicsfn Fatm_u(p::ConvectionLakePhysics{<:DefaultPhysics}, u,t) = -k_u(p,u,t)*(u[3]-Ceq_ch4(u[5],0)*1e6)
-@physicsfn Fatm_B(p::ConvectionLakePhysics{<:DefaultPhysics}, u,t) = -k_B(p,u,t)*(u[3]-Ceq_ch4(u[5],0)*1e6)
+@physicsfn Fatm(p::ConvectionLakePhysics{<:DefaultPhysics}, u,t) = -k(p,u,t)*(u[3]-Ceq_ch4(p,u[5],0)*1e6)
+@physicsfn Fatm_u(p::ConvectionLakePhysics{<:DefaultPhysics}, u,t) = -k_u(p,u,t)*(u[3]-Ceq_ch4(p,u[5],0)*1e6)
+@physicsfn Fatm_B(p::ConvectionLakePhysics{<:DefaultPhysics}, u,t) = -k_B(p,u,t)*(u[3]-Ceq_ch4(p,u[5],0)*1e6)
 
 # Wind penetration depth: deprecated!
 #########################################
