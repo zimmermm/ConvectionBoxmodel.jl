88cf81a9d4c052c93e6b5b45c0c4f32bf5140274
Matthias Zimmermann <zimmermm@hotmail.com>
2019-01-18 20:23:01 +0100

    fix interpolation compatibility

--- src/bathymetries.jl
+++ src/bathymetries.jl
@@ -7,7 +7,7 @@ Bathymetry Implementations
 surface_area(bathymetry::Interpolation{<:Any}) = bathymetry.y[1]
 volume_above(bathymetry::Interpolation{<:Any},depth; dh=0.01) = begin
 									grid = equigrid(top(bathymetry), depth, dh=dh)
-									areas_i = [bathymetry.at(z) for z in grid]
+									areas_i = bathymetry.at(grid)
 									sum((areas_i[1:end-1]+areas_i[2:end])/2.)*dh
 								end
 total_volume(bathymetry::Interpolation{<:Any}; dh=0.01) = volume_above(bathymetry, bottom(bathymetry), dh=dh)
