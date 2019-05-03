6fb318d1ec5db40be961571542ac33fb03af2dc8
Matthias Zimmermann <zimmermm@hotmail.com>
2019-05-03 21:56:26 +0200

    scale energy contributions

--- src/convectionlakephysics.jl
+++ src/convectionlakephysics.jl
@@ -320,7 +320,7 @@ const bi = [0.8181, -3.85e-3, 4.96e-5]
 # wind_speed
 @physicsfn wind_speed_at(p::ConvectionLakePhysics{<:DefaultPhysics}, u, t) =	begin
 																					if scenario.enabled & (u[1] > scenario.start_depth) & (t < scenario.scenario_end)
-																						scenario.eps_U10_emulator.at(u[1]*scenario.total_energy*scenario.wind_fraction/2.5)
+																						scenario.eps_U10_emulator.at(u[1]*scenario.total_energy*scenario.wind_fraction/2.5*κ)
 																					else
 																						f_wind*forcing.wind_speed.at(t)
 																					end
